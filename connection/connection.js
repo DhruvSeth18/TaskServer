a8988f9ff1bc87e41e20704c833943fe9aaa7f8b
Dhruv_Seth <dhruvseth090704@gmail.com>
2024-03-11 08:33:09 +0530

    correct mistakes

--- connection/connection.js
+++ connection/connection.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const makeConnection = async (username,password)=>{
     try{
-        await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.jb6pz7m.mongodb.net/?retryWrites=true&w=majority`);
-        console.log("Connection is successfull");
+        await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.jb6pz7m.mongodb.net/?retryWrites=true&w=majority`)
+        .then(()=>{console.log("Connection is successfull");})
     }
     catch(err){
         console.log("Error in the connection 🚨",err);
